89c018539e842aa22262ba7545779919c95fecf0
Dan Van Atta <dhvatta@gmail.com>
2018-03-25 22:20:06 -0700

    touch file properly"

--- system_control.sh
+++ system_control.sh
@@ -7,17 +7,17 @@ PATH=$PATH:/usr/sbin/
 . /root/infrastructure/common.sh
 
 
-CONTROL_FILE=$(find /root -maxdepth 1 -name ".control_lock" -type f -cmin -5)
+CONTROL_FILE_NAME=".control_lock"
+CONTROL_FILE=$(find /root -maxdepth 1 -name "${CONTROL_FILE_NAME}" -type f -cmin -5)
 if [ -z "$CONTROL_FILE" ]; then
-  rm -f $CONTROL_FILE
-  touch $CONTROL_FILE
+  touch ${CONTROL_FILE_NAME}
   report "Update has started"
   /root/infrastructure/root/tasks/install_authorized_root_keys.sh
   /root/infrastructure/root/tasks/install_triplea_user.sh
   /root/infrastructure/root/tasks/update_system_packages.sh
   /root/infrastructure/root/tasks/check_localhost.sh
   /root/infrastructure/root/tasks/enable_firewall.sh
-  rm $CONTROL_FILE
+  rm ${CONTROL_FILE_NAME}
   report "Update has completed"
 else
   report "Control file touch file found found, update progress is either still running or dead"
