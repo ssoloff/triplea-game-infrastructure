19e7a501f1a4b6232ee02399e151dc4ee28de832
Dan Van Atta <dhvatta@gmail.com>
2018-03-26 00:20:54 -0700

    More heavy-handed update, do a fresh clone on each time

--- root/root_cron.sh
+++ root/root_cron.sh
@@ -8,6 +8,6 @@ mkdir -p ${LOG_FOLDER}
 
 LOG_FILE="$LOG_FOLDER/root_cron_log.$(date +%Y).$(date +%m).$(date +%d)_$(date +%H).$(date +%M).log"
 echo "Cron executing ${date} ${hostname}" > $LOG_FILE
-
-git -C /root/infrastructure/ pull --rebase origin master > /dev/null
+rm -rf /root/infrastructure/
+git clone https://github.com/DanVanAtta/infrastructure.git 2>&1 >> ${LOG_FILE}
 /root/infrastructure/system_control.sh 2>&1 >> ${LOG_FILE}
