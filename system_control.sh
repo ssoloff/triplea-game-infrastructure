f991968fc85c0158ea67d55779b641407ffe594b
Dan Van Atta <dhvatta@gmail.com>
2018-03-25 22:39:11 -0700

    rm -f to avoid error when file does not exist

--- system_control.sh
+++ system_control.sh
@@ -43,7 +43,7 @@ function systemInstall() {
 
 ## set up an exit trap to make sure we remove the touch file on exit.
 function removeTouchFile() {
-  rm /root/CONTROL_FILE_NAME
+  rm -f /root/CONTROL_FILE_NAME
 }
 trap 'removeTouchFile' EXIT
 
