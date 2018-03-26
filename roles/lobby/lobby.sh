9ce4b4d09f6b685b7a95e7b7991a59c7690defc1
Dan Van Atta <dhvatta@gmail.com>
2018-03-26 00:40:38 -0700

    give up on the error reporting

--- roles/lobby/lobby.sh
+++ roles/lobby/lobby.sh
@@ -46,10 +46,6 @@ echo "Lobby port: ${PORT}"
 echo "Lobby database port: ${DATABASE_PORT}"
 echo "Lobby version tag: ${TAG_NAME}"
 
-if [[ -z "${PORT}" || -z "${DATABASE_PORT}" || "${TAG_NAME}" ]]; then
-  reportError "Required arg missing to lobby.sh"
-  exit 1
-fi
 
 # Retrieve latest triplea version
 if [[ "TAG_NAME" == "latest" ]]; then
