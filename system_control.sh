69cfdc86baf855355ec0958dbd54b7db7f124a68
Dan Van Atta <dhvatta@gmail.com>
2018-03-25 22:16:51 -0700

    Remake control file and report when we no-op"

--- system_control.sh
+++ system_control.sh
@@ -9,13 +9,15 @@ PATH=$PATH:/usr/sbin/
 
 CONTROL_FILE=$(find /root -maxdepth 1 -name ".control_lock" -type f -cmin -5)
 if [ -z "$CONTROL_FILE" ]; then
+  rm -f $CONTROL_FILE
   touch $CONTROL_FILE
-
   /root/infrastructure/root/tasks/install_authorized_root_keys.sh
   /root/infrastructure/root/tasks/install_triplea_user.sh
   /root/infrastructure/root/tasks/update_system_packages.sh
   /root/infrastructure/root/tasks/check_localhost.sh
   /root/infrastructure/root/tasks/enable_firewall.sh
   rm $CONTROL_FILE
+else
+  report "Control file touch file found found, update progress is either still running or dead"
 fi
 
