656729911350552551a36ff08ffe609caaecad7c
Dan Van Atta <dhvatta@gmail.com>
2018-03-26 23:22:23 -0700

    fix repo

--- setup/first_time_setup.sh
+++ setup/first_time_setup.sh
@@ -4,7 +4,7 @@ set -eux
 
 CLEAN="rm -rf /root/infrastructure/"
 ## TODO: update repo name from fork to triplea-game
-CLONE="git clone https://github.com/DanVanAtta/triplea.git /root/infrastructure/ &> /dev/null"
+CLONE="git clone https://github.com/DanVanAtta/infrastructure.git /root/infrastructure/ &> /dev/null"
 RUN_CRON="/root/infrastructure/update_cron.sh"
 
 sudo apt-get install -y git
