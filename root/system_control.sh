59067db50e0c3c0f09b4bba8185251bdd23c3a42
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 15:17:48 -0700

    fix bad name

--- root/system_control.sh
+++ root/system_control.sh
@@ -1,7 +1,7 @@
 /root/infrastructure/root/tasks/update_system_packages.sh
 /root/infrastructure/root/tasks/firewall.sh
 
-/root/infrastructure/root/tasks/install_authorized_root_keys.sh
+/root/infrastructure/root/tasks/user_ssh_keys.sh
 /root/infrastructure/root/tasks/_triplea_user.sh
 /root/infrastructure/root/tasks/prometheus_metrics_exporter.sh
 /root/infrastructure/root/tasks/papertrail.sh
