c9063212a122cf89ebc503dd42509a8b248f62de
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 13:42:50 -0700

    install papertrail once

--- root/tasks/install_papertrail.sh
+++ root/tasks/install_papertrail.sh
@@ -2,7 +2,9 @@
 
 . /root/infrastructure/common.sh
 
-PAPERTRAIL_TOKEN=$(readSecret "papertrail_token")
-
-wget -qO - --header="X-Papertrail-Token: ${PAPERTRAIL_TOKEN}" \
-https://papertrailapp.com/destinations/8364511/setup.sh | sudo bash
+## papertrail restarts rsyslogd, to avoid that we'll install papertrail only if needed
+if [ ! -f "/etc/rsyslog.d/95-papertrail.conf" ]; then
+  PAPERTRAIL_TOKEN=$(readSecret "papertrail_token")
+  wget -qO - --header="X-Papertrail-Token: ${PAPERTRAIL_TOKEN}" \
+    https://papertrailapp.com/destinations/8364511/setup.sh | sudo bash
+fi
