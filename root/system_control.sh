0db3abf2315ac1f5699d94b9beaba2270fa5fe11
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 15:27:45 -0700

    script typo

--- root/system_control.sh
+++ root/system_control.sh
@@ -3,7 +3,7 @@ set -eux
 /root/infrastructure/root/tasks/update_system_packages.sh
 /root/infrastructure/root/tasks/firewall.sh
 /root/infrastructure/root/tasks/user_ssh_keys.sh
-/root/infrastructure/root/tasks/_triplea_user.sh
+/root/infrastructure/root/tasks/triplea_user.sh
 /root/infrastructure/root/tasks/prometheus_metrics_exporter.sh
 /root/infrastructure/root/tasks/papertrail.sh
 /root/infrastructure/root/tasks/check_localhost.sh
