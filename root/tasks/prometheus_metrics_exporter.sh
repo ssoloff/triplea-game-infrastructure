67456000f973820b4fcf74df90afc7abb70c0857
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 15:35:07 -0700

    debug

--- root/tasks/prometheus_metrics_exporter.sh
+++ root/tasks/prometheus_metrics_exporter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+set -ex
+
 EXPORTER_URL="https://github.com/prometheus/node_exporter/releases/download/v0.16.0-rc.0/node_exporter-0.16.0-rc.0.linux-amd64.tar.gz"
-
-
 METRICS_SERVICE_FILE="/root/infrastructrure/root/files/metrics_export.service"
 METRICS_FOLDER="/home/metrics/node_exporter/"
 
