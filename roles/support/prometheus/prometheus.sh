7a03036faa46e76986edfd9d947dbb9e1394a853
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 16:40:28 -0700

    more fixing

--- roles/support/prometheus/prometheus.sh
+++ roles/support/prometheus/prometheus.sh
@@ -10,20 +10,22 @@ PROM_DL="https://github.com/prometheus/prometheus/releases/download/v2.2.1/prome
 PROM_CONFIG="/home/prometheus/prometheus.yml"
 
 
-
 installUser prometheus
-mkdir -p ${PROM_FOLDER}
-cp "/root/infrastructure/roles/support/prometheus/files/prometheus.yml" ${PROM_FOLDER}
 
 if [ ! -d "${PROM_FOLDER}" ]; then
   ufw allow 9090
   wget ${PROM_DL}
   tar xvfz prometheus-*.tar.gz
   rm *.tar.gz
-  mv prom*2.2.1* ${PROM_FOLDER}
-  installService prometheus ${PROM_SERVICE_FILE} ${PROM_FOLDER} "prometheus"
 
+  mv prom*2.2.1* ${PROM_FOLDER}
   echo "installed prometheus to ${PROM_FOLDER}"
 fi
+
+mkdir -p ${PROM_FOLDER}
+
+cp "/root/infrastructure/roles/support/prometheus/files/prometheus.yml" ${PROM_FOLDER}
+installService prometheus ${PROM_SERVICE_FILE} ${PROM_FOLDER} "prometheus"
+
 
 service prometheus start
