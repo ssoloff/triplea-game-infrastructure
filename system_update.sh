612c2a223111885704fb1e86b4b6d861cdb90f03
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 14:00:52 -0700

    add to path for root cron

--- system_update.sh
+++ system_update.sh
@@ -4,6 +4,8 @@ set -eux
 
 . /root/infrastructure/common.sh
 
+
 echo "Update Started"
+export PATH=${PATH}:/usr/local/sbin/:/usr/sbin/:/sbin/
 /root/infrastructure/root/system_control.sh
 /root/infrastructure/roles/host_control.sh
