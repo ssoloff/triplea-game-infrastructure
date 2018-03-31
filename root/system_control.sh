91df4cc198266ccaf98b3f1f524f96150f86d644
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 13:30:46 -0700

    install papertrail

--- root/system_control.sh
+++ root/system_control.sh
@@ -1,5 +1,6 @@
 /root/infrastructure/root/tasks/install_authorized_root_keys.sh
 /root/infrastructure/root/tasks/install_triplea_user.sh
 /root/infrastructure/root/tasks/update_system_packages.sh
+/root/infrastructure/root/tasks/install_papertrail.sh
 /root/infrastructure/root/tasks/check_localhost.sh
 /root/infrastructure/root/tasks/enable_firewall.sh
