28613f06848e0f2f8221e02830e40b159fa9895b
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 15:25:29 -0700

    fixes

--- root/system_control.sh
+++ root/system_control.sh
@@ -1,6 +1,7 @@
+set -eux
+
 /root/infrastructure/root/tasks/update_system_packages.sh
 /root/infrastructure/root/tasks/firewall.sh
-
 /root/infrastructure/root/tasks/user_ssh_keys.sh
 /root/infrastructure/root/tasks/_triplea_user.sh
 /root/infrastructure/root/tasks/prometheus_metrics_exporter.sh
