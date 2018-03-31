5572425e741c7435688108060343dc433a3bbb33
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 14:38:38 -0700

    prometheus metrics export on each host

--- roles/support/prometheus/prometheus.sh
+++ roles/support/prometheus/prometheus.sh
@@ -5,6 +5,8 @@ set -ex
 PROM_FOLDER="prometheus-2.2.1"
 
 if [ ! -d "${PROM_FOLDER}" ]; then
+
+  ufw enable 9090
   PROM_DL="https://github.com/prometheus/prometheus/releases/download/v2.2.1/prometheus-2.2.1.linux-amd64.tar.gz"
   wget ${PROM_DL}
   tar xvfz prometheus-*.tar.gz
@@ -12,3 +14,4 @@ if [ ! -d "${PROM_FOLDER}" ]; then
   mv prom*2.2.1* ${PROM_FOLDER}
   echo "installed prometheus to $(pwd)/${PROM_FOLDER}"
 fi
+
--- root/system_control.sh
+++ root/system_control.sh
@@ -1,6 +1,8 @@
-/root/infrastructure/root/tasks/install_authorized_root_keys.sh
-/root/infrastructure/root/tasks/install_triplea_user.sh
 /root/infrastructure/root/tasks/update_system_packages.sh
-/root/infrastructure/root/tasks/install_papertrail.sh
+/root/infrastructure/root/tasks/firewall.sh
+
+/root/infrastructure/root/tasks/install_authorized_root_keys.sh
+/root/infrastructure/root/tasks/_triplea_user.sh
+/root/infrastructure/root/tasks/prometheus_metrics_exporter.sh
+/root/infrastructure/root/tasks/papertrail.sh
 /root/infrastructure/root/tasks/check_localhost.sh
-/root/infrastructure/root/tasks/enable_firewall.sh
