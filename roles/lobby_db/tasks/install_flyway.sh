5b1fa6c212af5c1fa40b4998fb3bd0a82a597756
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 17:51:57 -0700

    mv flyway to home/triplea

--- roles/lobby_db/tasks/install_flyway.sh
+++ roles/lobby_db/tasks/install_flyway.sh
@@ -5,10 +5,13 @@ set -ex
 
 URL="https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-linux-x64.tar.gz"
 
+FLYWAY_FOLDER="/home/triplea/flyway-5.0.7"
 
 wget $URL
 tar xvf flyway*tar.gz
 rm *tar.gz
 
 
+mv flyway* ${FLYWAY_FOLDER}
+chown -R triplea:triplea /home/triplea/
 
