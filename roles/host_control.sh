dde9fa99435264177172e1e6d516415754bdf3b9
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 19:24:40 -0700

    some bash fu to get latest tag

--- roles/host_control.sh
+++ roles/host_control.sh
@@ -9,14 +9,22 @@ set -eux
 ## note: lobby IP addresses are not secret, but bot IP and database IP addresses are secret
 
 
-LATEST=$(curl -s 'https://api.github.com/repos/triplea-game/triplea/releases/latest' \
+LATEST_RELEASE=$(curl -s 'https://api.github.com/repos/triplea-game/triplea/releases/latest' \
     | python3 -c "import sys, json; print(json.load(sys.stdin)['tag_name'])")
+
+
+TEMP_FILE=$(tempfile)
+curl  -sq 'https://api.github.com/repos/triplea-game/triplea/tags' | \
+    grep "name" > $TEMP_FILE
+LATEST_BUILD=$(cat $TEMP_FILE | sed 's/.*\.//' | sed 's/".*//' | sort -nr | head -1)
+LATEST_TAG=$(grep ${LATEST_BUILD} $TEMP_FILE | sed 's/.* "//' | sed 's/".*$//')
+
 
 case "$(hostname)" in
   prerelease_staging)
-    ${LOBBY_DB} --port=5432 --tag-name=${LATEST}
-    ${LOBBY} --lobby-port 7000 --database-port 5000 --tag-name ${LATEST}
-    ${BOT} --start_port 8000 --count 2 --lobby-port 7000 --tag-name ${LATEST}
+    ${LOBBY_DB} --port=5432 --tag-name=${LATEST_TAG}
+    ${LOBBY} --lobby-port 7000 --database-port 5000 --tag-name ${LATEST_TAG}
+    ${BOT} --start_port 8000 --count 2 --lobby-port 7000 --tag-name ${LATEST_TAG}
     ;;
   infra-support)
     ${PROMETHEUS}
