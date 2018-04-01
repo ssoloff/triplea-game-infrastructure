6a94ebdacdeb620ee90e0ba5267bf815acd1f0aa
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 19:50:53 -0700

    fix

--- roles/lobby/tasks/install_lobby_artifacts.sh
+++ roles/lobby/tasks/install_lobby_artifacts.sh
@@ -8,6 +8,8 @@ function installLobby() {
   local destFolder=$1
   local tagName=$2
 
+
+
   echo "$tagName" > ${destFolder}/version
 
   local installerFile="triplea-${tagName}-server.zip"
@@ -16,10 +18,6 @@ function installLobby() {
   rm ${installerFile}
 
   chmod go-rw ${destFolder}/config/lobby/lobby.properties
-
-  cp ${RUN_LOBBY} ${destFolder}/
-  cp ${REMOVE_LOBBY} ${destFolder}/
-  chmod +x ${destFolder}/*.sh
 }
 
 installLobby $1 $2
