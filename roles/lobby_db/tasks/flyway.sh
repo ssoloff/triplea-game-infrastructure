ff87984bbc1f546062a1517d7161c9e6a985b3f4
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 18:02:19 -0700

    add trailing slash

--- roles/lobby_db/tasks/flyway.sh
+++ roles/lobby_db/tasks/flyway.sh
@@ -5,7 +5,7 @@ set -ex
 
 URL="https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-linux-x64.tar.gz"
 
-FLYWAY_FOLDER="/home/triplea/flyway-5.0.7"
+FLYWAY_FOLDER="/home/triplea/flyway-5.0.7/"
 
 if [ ! -d ${FLYWAY_FOLDER} ]; then
   wget $URL
