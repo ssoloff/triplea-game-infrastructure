882154e7be0688dad88bd52784405a8221d44a66
Dan Van Atta <dhvatta@gmail.com>
2018-03-31 18:17:58 -0700

    conditional create db

--- roles/lobby_db/tasks/install_postgres.sh
+++ roles/lobby_db/tasks/install_postgres.sh
@@ -3,3 +3,13 @@ set -ex
 . /root/infrastructure/common.sh
 
 apt -y postgresql postgresql-contrib
+
+## TODO: automate setting of password
+## sudo -u postgres psql postgres
+## \password postgres
+
+function createDb() {
+  echo "create database ta_users;" | sudo -u postgres psql postgres
+}
+
+echo "\l" | sudo -u postgres psql postgres | grep ta_users || createDb
