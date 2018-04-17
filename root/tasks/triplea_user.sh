a36c851aac58d1a96a50e96c9f4a13bce2dc9bb7
Dan Van Atta <dhvatta@gmail.com>
2018-04-16 21:31:38 -0700

    no password and add explicit restart bot perms to triplea user

--- root/tasks/triplea_user.sh
+++ root/tasks/triplea_user.sh
@@ -9,8 +9,11 @@ set -eu
 installUser triplea
 
 ## enable sudoer permissions
-egrep -q "^triplea.*triplea-bot" /etc/sudoers || echo "triplea ALL=(ALL) /usr/sbin/service triplea-bot" >> /etc/sudoers
-egrep -q "^triplea.*triplea-lobby" /etc/sudoers || echo "triplea ALL=(ALL) /usr/sbin/service triplea-lobby" >> /etc/sudoers
-egrep -q "^triplea.*journalctl" /etc/sudoers || echo "triplea ALL=(ALL) /usr/sbin/service journalctl" >> /etc/sudoers
-egrep -q "^triplea.*htop" /etc/sudoers || echo "triplea ALL=(ALL) /usr/bin/htop" >> /etc/sudoers
-egrep -q "^triplea.*iftop" /etc/sudoers || echo "triplea ALL=(ALL) /usr/bin/iftop" >> /etc/sudoers
+egrep -q "^triplea.*triplea-bot@01" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/sbin/service triplea-bot@01 restart" >> /etc/sudoers
+egrep -q "^triplea.*triplea-bot@02" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/sbin/service triplea-bot@02 restart" >> /etc/sudoers
+egrep -q "^triplea.*triplea-bot@03" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/sbin/service triplea-bot@03 restart" >> /etc/sudoers
+egrep -q "^triplea.*triplea-bot@04" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/sbin/service triplea-bot@04 restart" >> /etc/sudoers
+egrep -q "^triplea.*triplea-lobby" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/sbin/service triplea-lobby" >> /etc/sudoers
+egrep -q "^triplea.*journalctl" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/sbin/service journalctl" >> /etc/sudoers
+egrep -q "^triplea.*htop" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/bin/htop" >> /etc/sudoers
+egrep -q "^triplea.*iftop" /etc/sudoers || echo "triplea ALL=(ALL) NOPASSWD: /usr/bin/iftop" >> /etc/sudoers
