576552d6f462033554f2b8386829ee8942538f73
Dan Van Atta <dhvatta@gmail.com>
2018-05-16 21:51:20 -0700

    redirect logs to /home/admin and clean up some quoting (#55)

--- update_cron.sh
+++ update_cron.sh
@@ -14,20 +14,21 @@ CONTROL_FILE=$(find /root -maxdepth 1 -name "${CONTROL_FILE_NAME}" -type f -cmin
 
 ## set up an exit trap to make sure we remove the touch file on exit.
 function removeTouchFile() {
-  rm -f /root/${CONTROL_FILE_NAME}
+  rm -f "/root/${CONTROL_FILE_NAME}"
 }
 
 if [ -z "$CONTROL_FILE" ]; then
   MY_PID=$$
-  echo "${MY_PID}" > ${CONTROL_FILE_NAME}
+  echo "${MY_PID}" > "${CONTROL_FILE_NAME}"
   trap 'removeTouchFile' EXIT
 
-  LOG_FOLDER="/root/logs/$(date +%Y)/$(date +%m)/$(date +%d)/$(date +%H)"
+  LOG_FOLDER="/home/admin/logs/$(date +%Y)/$(date +%m)/$(date +%d)/$(date +%H)"
   LOG_FILE="$LOG_FOLDER/root_cron_log.$(date +%Y).$(date +%m).$(date +%d)_$(date +%H).$(date +%M).log"
   mkdir -p "$(dirname ${LOG_FILE})"
 
-  echo "Cron executing ${date} ${hostname}" > $LOG_FILE
-  /root/infrastructure/system_update.sh 2>&1 | tee -a ${LOG_FILE} | /usr/bin/logger
+  echo "Cron executing ${date} ${hostname}" > "${LOG_FILE}"
+  chown -R admin:admin "${LOG_FOLDER}"
+  /root/infrastructure/system_update.sh 2>&1 | tee -a "${LOG_FILE}" | /usr/bin/logger
 else
   report "Control file touch file found found, update progress is either still running or dead"
 fi
