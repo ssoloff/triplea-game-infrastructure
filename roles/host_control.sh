8f0c3cac656d2a239a1bf06058dfd93a373146c7
Ron <ron.murhammer@gmail.com>
2018-08-27 15:58:03 -0500

    Update number of bots

--- roles/host_control.sh
+++ roles/host_control.sh
@@ -21,7 +21,7 @@ PRERELEASE_LOBBY="66.175.213.79"
 PRERELEASE_LOBBY_PORT="7000"
 PRERELEASE_BOT_MEMORY=128
 
-BOT_COUNT=3
+BOT_COUNT=7
 BOT_MEMORY=168
 PROD_VERSION="1.9.0.0.10663"
 PROD_LOBBY_IP="45.79.144.53"
@@ -56,33 +56,11 @@ case "$(hostname)" in
       --lobby-host ${PRERELEASE_LOBBY} \
       --tag-name ${LATEST_TAG}
     ;;
-  bot25_ca_usa)
-    ${BOT} \
-      --bot-name CA_USA \
-      --bot-port 8000 \
-      --bot-start-number 2 \
-      --bot-count ${BOT_COUNT} \
-      --max-memory ${BOT_MEMORY} \
-      --lobby-port ${PROD_LOBBY_PORT} \
-      --lobby-host ${PROD_LOBBY_IP} \
-      --tag-name ${PROD_VERSION}
-    ;;
   bot35_frankfurt_de)
     ${BOT} \
       --bot-name FRANKFURT_DE \
       --bot-port 8000 \
       --bot-start-number 3 \
-      --bot-count 7 \
-      --max-memory ${BOT_MEMORY} \
-      --lobby-port ${PROD_LOBBY_PORT} \
-      --lobby-host ${PROD_LOBBY_IP} \
-      --tag-name ${PROD_VERSION}
-    ;;
-  bot45_atlanta_ga)
-    ${BOT} \
-      --bot-name ATLANTA_GA \
-      --bot-port 8000 \
-      --bot-start-number 4 \
       --bot-count ${BOT_COUNT} \
       --max-memory ${BOT_MEMORY} \
       --lobby-port ${PROD_LOBBY_PORT} \
@@ -105,7 +83,7 @@ case "$(hostname)" in
       --bot-name TOKYO_JP \
       --bot-port 8000 \
       --bot-start-number 6 \
-      --bot-count 7 \
+      --bot-count ${BOT_COUNT} \
       --max-memory ${BOT_MEMORY} \
       --lobby-port ${PROD_LOBBY_PORT} \
       --lobby-host ${PROD_LOBBY_IP} \
@@ -116,7 +94,7 @@ case "$(hostname)" in
       --bot-name NJ_USA_XL \
       --bot-port 8000 \
       --bot-start-number 7 \
-      --bot-count 7 \
+      --bot-count 12 \
       --max-memory 296 \
       --lobby-port ${PROD_LOBBY_PORT} \
       --lobby-host ${PROD_LOBBY_IP} \
