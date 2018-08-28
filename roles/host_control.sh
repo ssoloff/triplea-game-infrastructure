f832adf4444b1c030ecc1103211192d8af47df73
Ron <ron.murhammer@gmail.com>
2018-08-28 17:43:09 -0500

    Fix bot start number

--- roles/host_control.sh
+++ roles/host_control.sh
@@ -71,7 +71,7 @@ case "$(hostname)" in
     ${BOT} \
       --bot-name ATLANTA_GA \
       --bot-port 8000 \
-      --bot-start-number ${BOT_COUNT} \
+      --bot-start-number 4 \
       --bot-count ${BOT_COUNT} \
       --max-memory ${BOT_MEMORY} \
       --lobby-port ${PROD_LOBBY_PORT} \
