5ff93cdd828f014d2bdcc5c924a5d8cebacc801f
Dan Van_Atta <dhvatta@gmail.com>
2018-09-03 02:04:47 -0700

    Re-arrange variables, keep the more important and changing constants at top

--- roles/host_control.sh
+++ roles/host_control.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
-LOBBY_DB="/root/infrastructure/roles/lobby_db/lobby_db.sh --database-port 5432"
-LOBBY="/root/infrastructure/roles/lobby/lobby.sh --database-port 5432"
-BOT=/root/infrastructure/roles/bot/bot.sh
-PROMETHEUS=/root/infrastructure/roles/support/prometheus/prometheus.sh
-GRAFANA=/root/infrastructure/roles/support/grafana/grafana.sh
 
 set -eu
 . /root/infrastructure/common.sh
 
-TEMP_FILE=$(tempfile)
-curl  -sq 'https://api.github.com/repos/triplea-game/triplea/tags' | \
-    grep "name" > $TEMP_FILE
-LATEST_BUILD=$(cat $TEMP_FILE | sed 's/.*\.//' | sed 's/".*//' | sort -nr | head -1)
-LATEST_TAG=$(grep ${LATEST_BUILD} $TEMP_FILE | sed 's/.* "//' | sed 's/".*$//')
-
 PROD_LOBBY_IP="45.79.144.53"
 PROD_LOBBY_PORT="3304"
 PROD_LOBBY_VERSION="1.9.0.0.10663"
+
 PROD_BOT_VERSION="1.9.0.0.10663"
+PROD_BOT_MEMORY="168"
+PROD_BOT_COUNT="7"
+
+# pre-prod dynamically determines latest version
+PRE_PROD_LOBBY_IP="45.79.16.133"
+PRE_PROD_LOBBY_PORT="7000"
+
+DB_PORT="5432"
+
+
+LOBBY_DB="/root/infrastructure/roles/lobby_db/lobby_db.sh --database-port ${DB_PORT}"
+LOBBY="/root/infrastructure/roles/lobby/lobby.sh --database-port ${DB_PORT}"
+BOT=/root/infrastructure/roles/bot/bot.sh
+PROMETHEUS=/root/infrastructure/roles/support/prometheus/prometheus.sh
+GRAFANA=/root/infrastructure/roles/support/grafana/grafana.sh
+
 
 PROD_BOT="${BOT} \
-    --bot-port 8000 \
-    --bot-count 7 \
-    --max-memory 168 \
+    --bot-port ${PROD_BOT_COUNT} \
+    --bot-count ${PROD_BOT_COUNT} \
+    --max-memory ${PROD_BOT_MEMORY}  \
     --lobby-port ${PROD_LOBBY_PORT} \
     --lobby-host ${PROD_LOBBY_IP} \
     --tag-name ${PROD_BOT_VERSION}"
 
-PRE_PROD_LOBBY_PORT="7000"
-PRE_PROD_LOBBY_IP="45.79.16.133"
+TEMP_FILE=$(tempfile)
+# delete temp file on any exit event
+trap '{ rm -f ${TEMP_FILE}; }' EXIT
+
+## download github API data so we can scrape latest version information
+curl  -sq 'https://api.github.com/repos/triplea-game/triplea/tags' | \
+    grep "name" > $TEMP_FILE
+LATEST_BUILD=$(cat $TEMP_FILE | sed 's/.*\.//' | sed 's/".*//' | sort -nr | head -1)
+LATEST_TAG=$(grep ${LATEST_BUILD} $TEMP_FILE | sed 's/.* "//' | sed 's/".*$//')
 
 PRE_PROD_BOT="${BOT} \
       --bot-port 8000 \
